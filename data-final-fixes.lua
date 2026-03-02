27b0d3add5138ae36d5529f5729feac36501cbcc
SafTheLamb <safthelamb@gmail.com>
2026-03-01 21:33:20 -0800

    AAI Industry compat (glass tech)

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,5 +1,5 @@
 local ftech = require("__fdsl__.lib.technology")
 
-if not data.raw.technology["moshine-tech-glass"] or data.raw.technology["moshine-tech-glass"].hidden then
+if not data.raw.technology["moshine-tech-glass"] or data.raw.technology["moshine-tech-glass"].hidden or not data.raw.technology["moshine-tech-glass"].enabled then
 	ftech.remove_prereq("moshine-solaponics", "moshine-tech-glass")
 end
